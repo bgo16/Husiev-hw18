057eff65822fbc05e810abfbaede1aa5ac355db6
Bohdan Husiev <bgusev77@gmail.com>
2023-06-26 21:28:29 +0300

    qwdqwd

---
+++ script.js
@@ -0,0 +1,71 @@
+class Student {
+  constructor(name, surname, birthday) {
+    this.name = name;
+    this.surname = surname;
+    this.birthday = birthday;
+    this.grades = new Array(25);
+    this.attendance = Array.from({ length: 25 });
+    this.age = 2023 - this.birthday;
+  }
+
+  getAge() {
+    return this.age;
+  }
+
+  getMid() {
+    return (
+      this.grades.reduce((acc, el) => {
+        return acc + el;
+      }) / this.grades.length
+    );
+  }
+
+  present() {
+    if (this.attendance.includes(undefined)) {
+      this.attendance[this.attendance.indexOf(undefined)] = true;
+    } else console.log('More then 25');
+  }
+
+  absent() {
+    if (this.attendance.includes(undefined)) {
+      this.attendance[this.attendance.indexOf(undefined)] = false;
+    } else console.log('More then 25');
+  }
+
+  summary() {
+    const mid = this.getMid();
+    const midAtt =
+      this.attendance.filter(Boolean).length / this.attendance.length;
+    if (mid > 90 && midAtt > 0.9) {
+      return 'Молодець!';
+    } else if (mid > 90 || midAtt > 0.9) {
+      return 'Добре, але можна краще';
+    } else return 'Редиска!';
+  }
+
+  fill(a, b) {
+    for (let i = 0; i < this.grades.length; i++) {
+      this.grades[i] = Math.floor(Math.random() * 100) + a;
+    }
+    for (let i = 0; i < this.attendance.length - 1; i++) {
+      this.attendance[i] = Math.floor(Math.random() * b) ? true : false;
+    }
+  }
+}
+
+const student = new Student('Hoh', 'Kok', 2002);
+student.fill(50, 10);
+console.log(student.getAge());
+console.log(student.getMid());
+student.present();
+console.log(student.summary());
+
+console.log('===');
+
+const student2 = new Student('Hoh', 'Kok', 2003);
+student.fill(0, 2);
+console.log(student.getAge());
+console.log(student.getMid());
+student.present();
+student.present();
+console.log(student.summary());
